318b4d3c8b5a9a86e06f2875bc3dda2fcf939a79
MatildaZh <clounadaprodolzhaetsa@gmail.com>
2023-12-02 18:22:22 +0300

    класс Aeroflot, его методы и поля

---
+++ TechProg2/Aeroflot.cpp
@@ -0,0 +1,44 @@
+#include "Aeroflot.h"
+
+Aeroflot::Aeroflot() {
+
+	nameDestination = "";
+	numberFlight = 0;
+	typeAerocraft = "";
+}
+
+Aeroflot::~Aeroflot(){
+	std::cout << "Destructor AF\n";
+}
+
+void Aeroflot::setData() {
+
+	std::cout << "nameDestination: ";
+	std::cin >> this->nameDestination;
+	std::cout << '\n';
+
+	std::cout << "number: ";
+	while (!(std::cin >> this->numberFlight)) {
+		std::cout << "Trouble maaaann. Enter a number >0.\n";
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits <std::streamsize>::max(), '\n');
+		if (std::cin >> this->numberFlight)
+			break;
+	}
+	if (((this->numberFlight) < 0))
+		this->numberFlight = 0;
+	std::cout << '\n';
+
+	std::cout << "timeDeparture: ";
+	std::cin >> this->typeAerocraft;
+	std::cout << '\n';
+}
+
+
+void Aeroflot::getData() {
+	std::cout << "Aeroflot\n";
+	std::cout << "nameDestination: " << nameDestination << '\n';
+	std::cout << "number\t       : " << numberFlight << '\n';
+	std::cout << "typeAerocraft  : " << typeAerocraft << '\n';
+}
+
---
+++ TechProg2/Aeroflot.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+/*Определить класс с именем АЕRОFLОТ, содержащий следующие поля :
+● название пункта назначения рейса;
+● номер рейса;
+● тип самолета.
+Определить методы доступа к этим полям и перегруженные операции
+извлечения и вставки для объектов типа АЕRОFLОТ.*/
+
+#include <iostream>
+
+class Aeroflot {
+
+private:
+	std::string nameDestination;
+	int numberFlight;
+	std::string typeAerocraft;
+
+public:
+	Aeroflot();
+	~Aeroflot();
+
+	std::string& getNameDestination() {
+		return nameDestination;
+	}
+
+	int& getNumberFlight() {
+		return numberFlight;
+	}
+
+	std::string& getTypeAerocraft() {
+		return typeAerocraft;
+	}
+
+	void setNameDestination(std::string& str) {
+		nameDestination = str;
+	}
+
+	void setNumber(int& a) {
+		numberFlight = a;
+	}
+
+	void setTypeAerocraft(int& a) {
+		typeAerocraft = a;
+	}
+
+	void getData();
+	void setData();
+};
